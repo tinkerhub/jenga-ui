42ffbf3125e1a10a31dc5311b0de85771926b3aa
akhilmhdh <akhilmhdh@gmail.com>
2020-11-26 21:11:40 +0530

    added font family dmsans

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            fontFamily: {
+                dmsans: ['DMSans', 'sans-serif'],
+            },
             colors: {
                 primary: {
                     DEFAULT: '#007bff',
