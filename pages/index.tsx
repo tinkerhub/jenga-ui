2e301103bc4dd47ae7a7f5ee07af3baf1b6464c1
akhilmhdh <akhilmhdh@gmail.com>
2020-11-26 11:57:46 +0530

    added ts base

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div>hello world</div>
+        </div>
+    );
+};
+
+export default Home;
